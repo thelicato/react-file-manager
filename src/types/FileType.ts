7e91b3a37fcba80e1f38d4e04af6cb6e0615a54b
thelicato <thelicato@users.noreply.github.com>
2024-04-15 17:44:52 +0200

    chore: delete previous code structure

--- src/types/FileType.ts
+++
@@ -1,8 +0,0 @@
-// Be careful: even a folder is a file!
-export type FileType = {
-    id: string, // Unique ID given to this file
-    name: string,
-    isDir: boolean,
-    path?: string, // Optional because files inherit the path from the parentId folder
-    parentId?: string, // Optional because the root folder does not have a parent
-}
